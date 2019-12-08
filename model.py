6b9ad7fa066b7b9a6926367288c3709a0b3c6fbd
jmais <joseph.maisevich@ufl.edu>
2019-12-07 20:08:25 -0500

    defined getting top 10 in rows

--- model.py
+++ model.py
@@ -3,8 +3,17 @@ import sqlite3
 
 class Sightings:
     def __init__(self):
-        con = sqlite3.connect('flowers2019.db')
-        self.cur = con.cursor()
+        self.conn = sqlite3.connect('flowers2019.db')
+        self.cur = self.conn.cursor()
+
+    def __del__(self):
+        # body of destructor
+        self.conn.commit()
+        self.conn.close()
+
+    def getTop10(self,name)
+        rows = self.cur.execute("SELECT * from SIGHTINGS Where SIGHTINGS.NAME =? ORDER BY SIGHTINGS.sighted DESC LIMIT 10",(name,))
+        return rows
 
 
 
